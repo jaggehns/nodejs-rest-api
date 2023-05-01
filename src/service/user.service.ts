84d88360698c72a200c77685a011a073e22a47a5
Jaggehn <jaggehns@gmail.com>
2023-05-01 20:05:02 +0800

    Implemented user registration

---
+++ src/service/user.service.ts
@@ -0,0 +1,9 @@
+import UserModel, { UserDocument, UserInput } from "../models/user.model";
+
+export async function createUser(input: UserInput) {
+  try {
+    return await UserModel.create(input);
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
