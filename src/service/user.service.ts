277674c3626f3bbeb9c64d616503907e0ca44c78
Jaggehn <jaggehns@gmail.com>
2023-05-04 23:59:17 +0800

    Implemented sessions with refresh tokens

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,9 +1,39 @@
+import { FilterQuery } from "mongoose";
 import UserModel, { UserDocument, UserInput } from "../models/user.model";
+import { omit } from "lodash";
 
 export async function createUser(input: UserInput) {
   try {
-    return await UserModel.create(input);
+    const user = await UserModel.create(input);
+
+    console.log(user, "user");
+
+    return omit(user, "passsword");
   } catch (e: any) {
     throw new Error(e);
   }
+}
+
+export async function validatePassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    return false;
+  }
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) return false;
+
+  return omit(user.toJSON(), "passsword");
+}
+
+export async function findUser(query: FilterQuery<UserDocument>) {
+  return UserModel.findOne(query).lean();
 }
